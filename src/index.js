29a0febcbc1311f0b7003ca95a62274a1bc736ab
ReaganCn <rgncharana2@gmail.com>
2020-10-05 19:27:35 +0300

    Menu and header elements created

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import ReactDOM from "react-dom";
 import "../public/styles.css";
 
 const Main = (props) => {
-  return (
-<h1>Charana Reagan</h1>
-  );
+ 
 };
 
 ReactDOM.render(<Main />, document.getElementById("root"));
