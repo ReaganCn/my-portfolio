0b0b911d4eb48b90145c4add35dbe0f50881eed7
ReaganCn <rgncharana2@gmail.com>
2020-10-11 10:21:34 +0300

    added and animated react logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-     
+      animation: {
+        "spin-slow": "spin 4s linear infinite",
+      },
+      colors: {
+        "react-blue": "#00d8ff",
+      },
     },
     fontFamily: {
-      'space-mono': ['Space Mono']
-    }
+      "space-mono": ["Space Mono"],
+    },
   },
   variants: {},
   plugins: [],
-}
+};
