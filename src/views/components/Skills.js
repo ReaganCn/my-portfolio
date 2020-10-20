ff2e8f13bf83acec54e131d0f07ebf1a5f0cbf36
ReaganCn <rgncharana2@gmail.com>
2020-10-20 19:42:35 +0300

    implemented React custom and spinning logo

--- src/views/components/Skills.js
+++ src/views/components/Skills.js
@@ -6,8 +6,9 @@ const SkillsComponent = (props) => {
     <Fragment>
 
       
-        <div id="skill">
-          <img className="h-10" src={`public/imgs/skills_logos/${props.id}.png`} alt={`${props.name}`}/>
+        <div id="skill" className={`${props.id== 55 && "flex"}`}>
+          <img className={`h-10 ${props.id== 55 && "animate-spin-slow"}`} src={`public/imgs/skills_logos/${props.id}.png`} alt={`${props.name}`}/>
+          {props.id== 55 && <span className="self-center text-lg font-medium ml-3 text-react-blue tracking-wider"> React</span>}
         </div>
     
     </Fragment>
