dd487fb9f6beb6be622d3658304bcf4a966eab74
ReaganCn <rgncharana2@gmail.com>
2020-12-09 09:47:41 +0300

    adjusted skill image size and header margin for 4k screens

--- src/views/components/Skills.js
+++ src/views/components/Skills.js
@@ -1,16 +1,29 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 
 const SkillsComponent = (props) => {
-
   return (
     <Fragment>
-
-      
-        <div id="skill" className={`${props.id== 55 && "flex"} flex my-6 w-4/12 md:w-3/12 lg:w-auto md:my-4 lg:my-0 justify-center`}>
-          <img className={`md:h-10 md:w-auto w-auto h-8 justify-self-start ${props.id== 55 && "animate-spin-slow"}`} src={`public/imgs/skills_logos/${props.id}.png`} alt={`${props.name}`}/>
-          {props.id== 55 && <span className="self-center text-lg font-medium ml-3 text-react-blue tracking-wider"> React</span>}
-        </div>
-    
+      <div
+        id="skill"
+        className={`${
+          props.id == 55 && "flex"
+        } flex my-6 w-4/12 md:w-3/12 lg:w-auto md:my-4 lg:my-0 justify-center`}
+      >
+        <img
+          className={`md:h-10 md:w-auto w-auto h-8 justify-self-start ${
+            props.id == 55 && "animate-spin-slow"
+          }`}
+          src={`public/imgs/skills_logos/${props.id}.png`}
+          alt={`${props.name}`}
+          id="skill-image"
+        />
+        {props.id == 55 && (
+          <span className="self-center text-lg font-medium ml-3 text-react-blue tracking-wider">
+            {" "}
+            React
+          </span>
+        )}
+      </div>
     </Fragment>
   );
 };
