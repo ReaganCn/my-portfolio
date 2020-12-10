5d8cb8a39c278b3e059a2a29961410c398f56376
ReaganCn <rgncharana2@gmail.com>
2020-12-10 20:35:51 +0300

    changed email input border warning to 2px

--- src/views/components/Contact.js
+++ src/views/components/Contact.js
@@ -40,7 +40,7 @@ const ContactComponent = (props) => {
               name="email"
               value={props.data.email}
               onChange={props.onChange}
-              className= {`border ${props.validEmail ? "border-black" : "border-red-500" } p-1 w-full h-8 outline-none text-black`}
+              className= {` ${props.validEmail ? "border border-black" : "border-2 border-red-500" } p-1 w-full h-8 outline-none text-black`}
             />
           </span>
         </div>
