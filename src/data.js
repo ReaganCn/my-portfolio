19f69c6935313ce688c144618915ebd25eb26b0f
Reagan Charana <34684844+ReaganCn@users.noreply.github.com>
2023-05-12 10:30:42 +0300

    chore: update projects

--- src/data.js
+++ src/data.js
@@ -1,6 +1,47 @@
 const Projects = [
+
   {
-    id: 26,
+    id: 25,
+    icon: "fas fa-globe-americas",
+    title: "DomainCatch",
+    description:
+      "A domain Catching Service for expired domains and a custom TLS protocol to ensure secure communication. The project involved creating an efficient and scalable service that can quickly catch expired domains and make them available for purchase.",
+    technologies: ["Golang","Google Cloud Platform", "Nodejs"],
+    github: "",
+    live: ""
+  },
+  {
+    id: 24,
+    icon: "fas fa-code",
+    title: "TeleGPT",
+    description:
+      "An integration of openai’s chat gpt on Telegram to ensure easy accessibility and features such as voice",
+    technologies: ["Golang", "AWS","Docker", "Amazon ECS", "MongoDB"],
+    github: "https://github.com/ReaganCn/telegram-gpt",
+    live: ""
+  },
+  {
+    id: 23,
+    icon: "fas fa-play-circle",
+    title: "Filewatch",
+    description:
+      "A file monitoring Golang command line tool that can watch for modifications, creations and deletions of files and folders and send an event",
+    technologies: ["Golang","AWS", "Docker","Amazon ECS","SQLite"],
+    github: "https://github.com/ReaganCn/filewatch",
+    live: ""
+  },
+  {
+    id: 22,
+    icon: "fas fa-code",
+    title: "Elewa Conversational Learning Manager",
+    description:
+      "A web application developed to empower individuals and organizations with the ability to design bots without requiring a technical background.",
+    technologies: ["NodeJs","Golang", "Google Cloud Platform"],
+    github: "https://github.com/ReaganCn/gravevideos-nodejs",
+    live: ""
+  },
+  {
+    id: 21,
     icon: "fas fa-address-card",
     title: "My portfolio",
     description:
@@ -8,57 +49,7 @@ const Projects = [
     technologies: ["HTML5","Javascript", "React Js", "Tailwind CSS"],
     github: "https://github.com/ReaganCn/my-portfolio",
     live: "https://charanareagan.netlify.app/"
-  },
-  {
-    id: 25,
-    icon: "fas fa-globe-americas",
-    title: "Shortly URL Shortener Front End",
-    description:
-      "The front end to complement the URL shortener API for easy usability",
-    technologies: ["HTML5","Javascript", "ReactJs", "ReduxJs","Webpack", "Tailwind CSS"],
-    github: "https://github.com/ReaganCn/short-url",
-    live: "https://shortly-project.netlify.app/"
-  },
-  {
-    id: 24,
-    icon: "fas fa-code",
-    title: "Shortly URL Shortener API",
-    description:
-      "Handles user authetication, URL shortening and URL redirect among other API requests for the Shortly Project",
-    technologies: ["Javascript", "NodeJs(Express)","Passport Js Auth", "Mongo DB", "Hosted on Digital Ocean"],
-    github: "https://github.com/ReaganCn/shorturl-nodejs",
-    live: ""
-  },
-  {
-    id: 23,
-    icon: "fas fa-play-circle",
-    title: "Gravel Videos",
-    description:
-      "A concept of a programming tutorial website whereby learning is broken into the simplest and smallest bits",
-    technologies: ["HTML5","Javascript", "ReactJs","ReduxJs","Webpack", "Bootstrap 4"],
-    github: "https://github.com/ReaganCn/gravel-videos",
-    live: "https://gravel-videos.netlify.app/"
-  },
-  {
-    id: 22,
-    icon: "fas fa-code",
-    title: "Gravel Videos API",
-    description:
-      "Serves the Gravel Videos Front End by handling video files, extracting the necessary meta data and saving them to the server and the info to the database",
-    technologies: ["Javascript", "NodeJs(Express)","Mongo DB", "Hosted on Glitch.com"],
-    github: "https://github.com/ReaganCn/gravevideos-nodejs",
-    live: ""
-  },
-  {
-    id: 21,
-    icon: "fas fa-quote-left",
-    title: "Random Quote Generator",
-    description:
-      "An application that shows random quotes based on a basic API request with simple animations. Utilizes the Twitter API for sharing.",
-    technologies: ["HTML5","Javascript", "ReactJs","Bootstrap 4", "Twitter API"],
-    github: "https://github.com/ReaganCn/random-quotes",
-    live: "https://get-random-quotes.netlify.app/"
-  },
+  }
 ];
 
 const Skills = [
