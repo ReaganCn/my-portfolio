f9bf17cbfca16ddd38ffa4f646ed5a3209765011
Reagan Charana <34684844+ReaganCn@users.noreply.github.com>
2023-08-24 14:39:12 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -5,23 +5,43 @@ const Projects = [
     icon: "fas fa-globe-americas",
     title: "DomainCatch",
     description:
-      "A domain Catching Service for expired domains and a custom TLS protocol to ensure secure communication. The project involved creating an efficient and scalable service that can quickly catch expired domains and make them available for purchase.",
-    technologies: ["Golang","Google Cloud Platform", "Nodejs"],
+      "The project involved creating an efficient and scalable service that can quickly catch expired domains and make them available for purchase through an easy to navigate platform showcasing domains for sale and for bidding.",
+    technologies: ["Angular 14","Typescript", "Nodejs"],
     github: "",
     live: ""
   },
   {
     id: 24,
     icon: "fas fa-code",
-    title: "TeleGPT",
+    title: "Spotlights",
     description:
-      "An integration of openai’s chat gpt on Telegram to ensure easy accessibility and features such as voice",
-    technologies: ["Golang", "AWS","Docker", "Amazon ECS", "MongoDB"],
+      "Lead development of an MVP for an hr tech startup that enabled recruiters to upload CVs of 'second best' candidates, providing companies with access to quality talent. The MVP was built using Angular and Node.js",
+    technologies: ["Angular", "GCP","NodeJs"],
     github: "https://github.com/ReaganCn/telegram-gpt",
     live: ""
   },
   {
     id: 23,
+    icon: "fas fa-code",
+    title: "Elewa Conversational Learning Manager",
+    description:
+      "A web application developed to empower individuals and organizations with the ability to design bots without requiring a technical background.",
+    technologies: ["Angular","NodeJs", "Google Cloud Platform"],
+    github: "https://github.com/italanta/elewa",
+    live: ""
+  },
+  {
+    id: 22,
+    icon: "fas fa-address-card",
+    title: "My portfolio",
+    description:
+      "This website is a redesign of my previous portfolio for speed and efficiency",
+    technologies: ["HTML5","Javascript", "React Js", "Tailwind CSS"],
+    github: "https://github.com/ReaganCn/my-portfolio",
+    live: "https://charanareagan.netlify.app/"
+  },
+    {
+    id: 21,
     icon: "fas fa-play-circle",
     title: "Filewatch",
     description:
@@ -30,26 +50,6 @@ const Projects = [
     github: "https://github.com/ReaganCn/filewatch",
     live: ""
   },
-  {
-    id: 22,
-    icon: "fas fa-code",
-    title: "Elewa Conversational Learning Manager",
-    description:
-      "A web application developed to empower individuals and organizations with the ability to design bots without requiring a technical background.",
-    technologies: ["NodeJs","Golang", "Google Cloud Platform"],
-    github: "https://github.com/italanta/elewa",
-    live: ""
-  },
-  {
-    id: 21,
-    icon: "fas fa-address-card",
-    title: "My portfolio",
-    description:
-      "This website is a redesign of my previous portfolio for speed and efficiency",
-    technologies: ["HTML5","Javascript", "React Js", "Tailwind CSS"],
-    github: "https://github.com/ReaganCn/my-portfolio",
-    live: "https://charanareagan.netlify.app/"
-  }
 ];
 
 const Skills = [
