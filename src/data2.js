61e32047e9e740899560a463d904ee7005a053f3
Reagan Charana <34684844+ReaganCn@users.noreply.github.com>
2023-08-24 14:33:29 +0300

    Update data2.js

--- src/data2.js
+++ src/data2.js
@@ -1,1 +1,91 @@
 
+const Projects = [
+
+  {
+    id: 25,
+    icon: "fas fa-globe-americas",
+    title: "DomainCatch",
+    description:
+      "A domain Catching Service for expired domains and a custom TLS protocol to ensure secure communication. The project involved creating an efficient and scalable service that can quickly catch expired domains and make them available for purchase.",
+    technologies: ["Golang","Google Cloud Platform", "Nodejs"],
+    github: "",
+    live: ""
+  },
+  {
+    id: 24,
+    icon: "fas fa-code",
+    title: "TeleGPT",
+    description:
+      "An integration of openai’s chat gpt on Telegram to ensure easy accessibility and features such as voice",
+    technologies: ["Golang", "AWS","Docker", "Amazon ECS", "MongoDB"],
+    github: "https://github.com/ReaganCn/telegram-gpt",
+    live: ""
+  },
+  {
+    id: 23,
+    icon: "fas fa-play-circle",
+    title: "Filewatch",
+    description:
+      "A file monitoring Golang command line tool that can watch for modifications, creations and deletions of files and folders and send an event",
+    technologies: ["Golang","AWS", "Docker","Amazon ECS","SQLite"],
+    github: "https://github.com/ReaganCn/filewatch",
+    live: ""
+  },
+  {
+    id: 22,
+    icon: "fas fa-code",
+    title: "Elewa Conversational Learning Manager",
+    description:
+      "A web application developed to empower individuals and organizations with the ability to design bots without requiring a technical background.",
+    technologies: ["NodeJs","Golang", "Google Cloud Platform"],
+    github: "https://github.com/italanta/elewa",
+    live: ""
+  },
+  {
+    id: 21,
+    icon: "fas fa-address-card",
+    title: "My portfolio",
+    description:
+      "This website is a redesign of my previous portfolio for speed and efficiency",
+    technologies: ["HTML5","Javascript", "React Js", "Tailwind CSS"],
+    github: "https://github.com/ReaganCn/my-portfolio",
+    live: "https://charanareagan.netlify.app/"
+  }
+];
+
+const Skills = [
+  {
+    id: 51,
+    name: "Golang",
+  },
+  {
+    id: 52,
+    name: "NodeJs",
+  },
+  {
+    id: 53,
+    name: "AWS",
+  },
+  {
+    id: 54,
+    name: "Docker",
+  },
+  {
+    id: 55,
+    name: "Kubernetes",
+  },
+  {
+    id: 56,
+    name: "GCP",
+  },
+  {
+    id: 58,
+    name: "MongoDB",
+  },
+  {
+    id: 59,
+    name: "SQL",
+  },
+];
+
+export { Projects, Skills };
